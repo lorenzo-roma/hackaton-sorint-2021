b83b61f57688217181d166c5bd9d0d378e872a53
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-09-18 14:43:35 +0200

    adding autocomplete google maps

--- frontend/src/components/SelectAddress.component.tsx
+++ frontend/src/components/SelectAddress.component.tsx
@@ -1,3 +1,37 @@
-const SelectAddress = () => {};
+const SelectAddress = () => {
+    return (
+        <div>
+            <script
+                async
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCEE4TBbznhMAw6igFIl_sq_NHqkYgd9go&libraries=places&callback=initAutocomplete"
+            ></script>
+            <script lang="js">
+                {`let autocomplete;
+                console.log("completed");
+                function initAutoComplete(){
+                    autocomplete = new google.maps.places.AutoComplete(
+                        document.getElementById('autocomplete'),
+                        {
+                            componentRestrictions: {"country": ["IT"]},
+                            fields: ["place_id", 'geometry', "name"]
+                        }
+                    );
+                    autocomplete.addListener("place_changed", onPlaceChanged);
+                };
+                
+                function onPlaceChanged(){
+                    let place = autocomplete.getPlace();
+                    if(!place.geometry){
+                        document.getElementById("autocomplete").placeholder = "Enter a place";
+                    } else {
+                        document.getElementById("details").innerHTML = place.name;
+                    }
+
+                };`}
+            </script>
+            <input id="autocomplete" placeholder="Enter a place" type="text" />
+        </div>
+    );
+};
 
 export default SelectAddress;
--- frontend/src/pages/HomePagePrivate.page.tsx
+++ frontend/src/pages/HomePagePrivate.page.tsx
@@ -1,50 +1,56 @@
-import {useTripListMutation} from "../services/trip.service";
-import {useEffect} from "react";
+import { useTripListMutation } from "../services/trip.service";
+import { useEffect } from "react";
 import LoadingComponent from "../components/Loading.component";
 import ConfirmedTrip from "../classes/ConfirmedTrip.class";
 import ErrorComponent from "../components/Error.component";
-import {useSelector} from "react-redux";
-import {useAppSelector} from "../stores/store";
-import TripWizard, {TripWizardProps} from "../components/TripWizard.component";
+import { useSelector } from "react-redux";
+import { useAppSelector } from "../stores/store";
+import TripWizard, {
+    TripWizardProps,
+} from "../components/TripWizard.component";
+import SelectAddress from "../components/SelectAddress.component";
 
 type HomePagePrivateProps = TripWizardProps;
 
 const HomePagePrivate = (props: HomePagePrivateProps) => {
-    const [doTrips, {isLoading, isError, data: tripsResponse, isSuccess}] = useTripListMutation();
+    const [doTrips, { isLoading, isError, data: tripsResponse, isSuccess }] =
+        useTripListMutation();
     useEffect(() => {
         doTrips();
-    }, [doTrips])
-    if (isLoading)
-        return <LoadingComponent/>;
-    if(isSuccess) console.log(tripsResponse)
+    }, [doTrips]);
+    if (isLoading) return <LoadingComponent />;
+    if (isSuccess) console.log(tripsResponse);
     return (
         <div>
-            <TripWizard initialFrom={props.initialFrom} initialTo={props.initialTo} />
-            {isError && <ErrorComponent error={"Error while loading trips"}/>}
+            <SelectAddress />
+            <TripWizard
+                initialFrom={props.initialFrom}
+                initialTo={props.initialTo}
+            />
+            {isError && <ErrorComponent error={"Error while loading trips"} />}
             {isSuccess && (
                 <>
                     <h1>Upcoming Trips</h1>
-                    {
-                        tripsResponse && tripsResponse.confirmedTrips.map((trip) => (
+                    {tripsResponse &&
+                        tripsResponse.confirmedTrips.map((trip) => (
                             <div>
-                                {trip.confirmedPickedUp} {trip.from} to {trip.to} {trip.arrival}
+                                {trip.confirmedPickedUp} {trip.from} to{" "}
+                                {trip.to} {trip.arrival}
                             </div>
-                        ))
-                    }
+                        ))}
                     <h1>To Be Scheduled Trip</h1>
-                    {
-                        tripsResponse && tripsResponse.toBeScheduledTrips.map((trip) => (
+                    {tripsResponse &&
+                        tripsResponse.toBeScheduledTrips.map((trip) => (
                             <div>
-                                {trip.initialAvailability}-{trip.endAvailability} {trip.from} to {trip.to} {trip.arrival}
+                                {trip.initialAvailability}-
+                                {trip.endAvailability} {trip.from} to {trip.to}{" "}
+                                {trip.arrival}
                             </div>
-                        ))
-                    }
-
+                        ))}
                 </>
-            )
-            }
+            )}
         </div>
     );
-}
+};
 
 export default HomePagePrivate;
