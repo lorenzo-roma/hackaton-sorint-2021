dde74fcff4d754d2fed7bc5287fbfe4724e5d58c
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-09-18 21:38:59 +0200

    adding tests

--- api/tests/services/provider.test.ts
+++ api/tests/services/provider.test.ts
@@ -1,6 +1,8 @@
 jest.mock("node-fetch");
 import AuthService from "../../services/auth/auth-service";
 import AuthServiceInterface from "../../services/auth/auth-service-interface";
+import PathOptimizer from "../../services/path-optimizer/path-optimizer";
+import PathOptimizerServiceInterface from "../../services/path-optimizer/path-optimizer-interface";
 import PathService from "../../services/path/path-service";
 import PathServiceInterface from "../../services/path/path-service-interface";
 import ServiceProvider from "../../services/provider";
@@ -53,5 +55,35 @@ describe("Provide service instances", () => {
         const secondShiftService: ShiftServiceInterface =
             ServiceProvider.getShiftService();
         expect(secondShiftService).toBe(shiftService);
+    });
+
+    test("Provide valid path service", () => {
+        const pathService: PathServiceInterface =
+            ServiceProvider.getPathService();
+        expect(pathService).toBeTruthy();
+        expect(pathService).toBeInstanceOf(PathService);
+    });
+
+    test("Provide always the same instance of path service", () => {
+        const pathService: PathServiceInterface =
+            ServiceProvider.getPathService();
+        const secondPathService: PathServiceInterface =
+            ServiceProvider.getPathService();
+        expect(pathService).toBe(secondPathService);
+    });
+
+    test("Provide valid optimize path service", () => {
+        const optimizeService: PathOptimizerServiceInterface =
+            ServiceProvider.getOptimizerService();
+        expect(optimizeService).toBeTruthy();
+        expect(optimizeService).toBeInstanceOf(PathOptimizer);
+    });
+
+    test("Provide always the same instance of optimized path service", () => {
+        const optimizeService: PathOptimizerServiceInterface =
+            ServiceProvider.getOptimizerService();
+        const secondOptimizeService: PathOptimizerServiceInterface =
+            ServiceProvider.getOptimizerService();
+        expect(optimizeService).toBe(secondOptimizeService);
     });
 });
