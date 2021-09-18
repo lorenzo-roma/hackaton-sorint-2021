b2b2701c71299aaedb2f4d302fb68a4871d55ded
DCampagnola <dcampagnola.dev@gmail.com>
2021-09-18 14:40:19 +0200

    Updated frontend

--- frontend/src/classes/ConfirmedTrip.class.ts
+++ frontend/src/classes/ConfirmedTrip.class.ts
@@ -1,15 +1,7 @@
 import Trip from "./trip.class";
 import {TripResult} from "../services/trip.service";
 
-export default class ConfirmedTrip extends Trip {
-    constructor(id: number, from: string, to: string,
-                arrival: Date,
-                public confirmedPickedUp: Date,) {
-        super(id, from, to, arrival);
-    }
+export default interface ConfirmedTrip extends Trip {
+    confirmedPickedUp:Date;
 
-
-    static fromTripResult(tripResult: TripResult) {
-        return new ConfirmedTrip(tripResult.id, tripResult.from, tripResult.to, tripResult.arrival!, tripResult.confirmedPickup!)
-    }
 }
--- frontend/src/classes/ToBeScheduledTrip.class.ts
+++ frontend/src/classes/ToBeScheduledTrip.class.ts
@@ -1,16 +1,11 @@
 import Trip from "./trip.class";
 import {TripResult} from "../services/trip.service";
 
-export default class ToBeScheduledTrip extends  Trip  {
-    constructor(id:number, from: string, to:string, arrival: Date, public initialAvailability: Date, public endAvailability: Date) {
-        super(id, from, to, arrival);
-    }
+export default interface ToBeScheduledTrip extends Trip {
 
-    static fromTripResult(tripResult: TripResult) {
-        return new ToBeScheduledTrip(tripResult.id, tripResult.from, tripResult.to, tripResult.arrival, tripResult.initialAvailability!, tripResult.endAvailability!)
-    }
+    initialAvailability: Date;
+    endAvailability: Date;
 }
-
 export interface ToBeScheduledTripApiInterface {
     from: string;
     to: string;
