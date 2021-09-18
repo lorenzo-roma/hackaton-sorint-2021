4e31c80a26408911f0401c3cd6220d9913f8c3b3
DCampagnola <dcampagnola.dev@gmail.com>
2021-09-18 17:58:26 +0200

    Added path-optimizer.ts

---
+++ api/services/path/path-optimizer.ts
@@ -0,0 +1,57 @@
+import fetch from 'node-fetch';
+
+export default class PathOptimizer {
+    static basePath = "https://api.routexl.com";
+
+    static async callDistanceMatrix(locations: Location[]) {
+        const params = new URLSearchParams();
+        params.set('locations', JSON.stringify(locations));
+        const result = await fetch(`${this.basePath}/distance`, {
+            method: 'POST',
+            body: params
+        });
+        const r = (await result.json()) as DistanceMatrixResponse;
+        return {
+            count: r.count,
+            distances: Object.values(r.distances)
+        } as DistanceMatrix
+    }
+
+    static async getDistancesToLocations(startingLocation: Location, endingLocations: Location[]) {
+        const result = await this.callDistanceMatrix([startingLocation, ...endingLocations]);
+        return result.distances.filter(distance => distance.from === "1");
+    }
+}
+
+export interface DistanceMatrixResponse {
+    count: number;
+    distances:
+        {
+            [index: string]:
+                {
+                    from: string;
+                    to: string;
+                    distance: number;
+                    duration: number;
+                    router: boolean;
+                }
+        }
+}
+
+export interface DistanceMatrix {
+    count: number;
+    distances:
+        {
+            from: string;
+            to: string;
+            distance: number;
+            duration: number;
+            router: boolean;
+        }[]
+}
+
+export interface Location {
+    longitude: number;
+    latitude: number;
+    id: number;
+}
