d8d134130310adcd34d8532fffe29baccd0695f0
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-09-19 14:18:18 +0200

    improving design

--- frontend/src/pages/auth/Login.page.tsx
+++ frontend/src/pages/auth/Login.page.tsx
@@ -6,6 +6,7 @@ import useInput from "../../hooks/useInput.hook";
 import { NOT_EMPTY_STRING } from "../../utils/Validators";
 import { useAppDispatch } from "../../stores/store";
 import { useCookies } from "react-cookie";
+import Footer from "../../components/Footer";
 import { setToken } from "../../stores/auth.store";
 import { useLoginMutation } from "../../services/auth.service";
 type LoginPageProps = {
@@ -34,21 +35,33 @@ const LoginPage = (props: LoginPageProps) => {
     };
 
     return (
-        <div className="mx-4 h-75 row align-items-center">
-            <div className="col">
-                <Card className="p-4">
-                    <FormGroup className="sanserif">
-                        <label className="serif black fs-6 mb-1">
-                            Username
-                        </label>
+        <div className="row" style={{ height: "80vh" }}>
+            <div className="h-100 col-6 header1 ">
+                <div className="w-75 h-100 d-flex flex-column justify-content-center mx-auto">
+                    <div className="h-50">
+                        Hop In,<br></br>
+                        Hop off<br></br>
+                        <div className="body2">
+                            Rethink your way to travel daily
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="h-100 col-6 flex-column d-flex justify-content-center">
+                <Card className="p-4 w-75 bg-primary-light">
+                    <div className="header1">Login</div>
+                    <div className="body1 mt-2">
+                        You don't have an account?{" "}
+                        <span className="button-font">Sign up!</span>
+                    </div>
+                    <FormGroup>
+                        <label className="body2 fs-6 mt-4 mb-1">Username</label>
                         <InputText
                             type="text"
                             placeholder="Username"
                             {...usernameInput}
                         />
-                        <label className="serif black fs-6 mt-3 mb-1">
-                            Password
-                        </label>
+                        <label className="body2 fs-6 mt-3 mb-1">Password</label>
                         <InputText
                             type="password"
                             placeholder="Password"
@@ -59,12 +72,15 @@ const LoginPage = (props: LoginPageProps) => {
                         {isError && (
                             <ErrorComponent error="Error during login" />
                         )}
-                        <div className="mt-4">
-                            <Button onClick={onLoginClicked}>Login</Button>
+                        <div className="mt-4 fs-6 d-flex row justify-content-center">
+                            <div className="w-50 mt-4 text-center d-flex row button-font">
+                                <Button onClick={onLoginClicked}>Login</Button>
+                            </div>
                         </div>
-                    </FormGroup>
+                    </FormGroup>{" "}
                 </Card>
             </div>
+            <Footer />
         </div>
     );
 };
