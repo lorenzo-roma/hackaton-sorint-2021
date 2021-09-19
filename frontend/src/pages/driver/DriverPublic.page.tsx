674d0cf11d05b0d9c3db0800b2c533d2aac0c53b
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-09-19 15:39:58 +0200

    added driver image

--- frontend/src/pages/driver/DriverPublic.page.tsx
+++ frontend/src/pages/driver/DriverPublic.page.tsx
@@ -1,24 +1,40 @@
-import {Button} from "../../components/system/InputText";
-import {useState} from "react";
-import {Redirect} from "react-router-dom";
-import {Col, Container, Row} from "react-bootstrap";
+import { Button } from "../../components/system/InputText";
+import { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
+import img from "../../assets/driver.jpg";
 
 const DriverPublicPage = () => {
     const [redirect, setRedirect] = useState(false);
-    if (redirect) return <Redirect to="/driver/signup"/>
-    return (<Container className=" h-100 w-100">
-        <Row className="vh-100">
-            <Col xs="12" md="6">
-                <div style={{marginTop: '30vh'}}>
-                    <h1 className="header1">Create a Business</h1>
-                    <p className="body1">You only need a van and schedule some shifts</p>
-                    <Button onClick={() => setRedirect(true)}>JOIN US</Button>
-                </div>
-            </Col>
-            <Col xs="12" md="6" className="bg-primary align-self-center" style={{height: '60vh'}}>
-
-            </Col>
-        </Row>
-    </Container>);
-}
+    if (redirect) return <Redirect to="/driver/signup" />;
+    return (
+        <Container className=" h-100 w-100">
+            <Row className="vh-100">
+                <Col xs="12" md="6">
+                    <div style={{ marginTop: "30vh" }}>
+                        <h1 className="header1">Create a Business</h1>
+                        <p className="body1">
+                            You only need a van and schedule some shifts
+                        </p>
+                        <Button onClick={() => setRedirect(true)}>
+                            JOIN US
+                        </Button>
+                    </div>
+                </Col>
+                <Col
+                    xs="12"
+                    md="6"
+                    className="bg-primary align-self-center"
+                    style={{
+                        height: "60vh",
+                        backgroundImage: `url(${img})`,
+                        backgroundPosition: "center",
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "contain",
+                    }}
+                ></Col>
+            </Row>
+        </Container>
+    );
+};
 export default DriverPublicPage;
